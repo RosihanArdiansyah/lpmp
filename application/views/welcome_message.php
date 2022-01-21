f23ef87ddf19aa0d3ca68d8ad09e803f5f2cd6cd
Rosihan Ardiansyah <rosihan17@gmail.com>
2022-01-21 12:11:54 +0800

    no rickroll :(

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -116,15 +116,19 @@
 
 				<!-- HOME SECTION
 				================================================== -->
-				<section id="homepage" class="home page-section parallax-2 overlay-light-alpha-10" style="padding-top: 512px ;">
+				<section id="homepage" class="home page-section parallax-2 overlay-light-alpha-10" style="padding-top: 32px ;">
+				
 				<video autoplay muted loop id="myVideo" style="position:fixed;right:0;bottom:0;min-width:100%;min-height:100%;">
-  							<source src="rickroll.mp4" type="video/mp4">
+  							<source src="LPMP_vids_revisi.mp4" type="video/mp4">
 					</video>		
 					<div class="table-content columns">	
 						<div class="table-center-text">
-							<div class="container">		
+							<div class="row">
+								<img src="lpmp.png" width="60%" style="background-color:transparent;">
+							</div>
+								<div class="container">		
 
-								<div class="local-scroll" style="padding-top: 8px;">
+									<div class="local-scroll" style="padding-top: 128px;">
 										<a href="http://kemdikbud.go.id/main/?lang=id" target="_blank"
 											title="Kementerian Pendidikan dan Kebudayaan"><img
 											src="<?=base_url()?>assets/images/kemendikbud.jpg" width="200px"
